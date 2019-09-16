ab821219905a0eeca86d5669659305e78bdaa2d1
Becca <beccaburns89@gmail.com>
2019-09-16 15:42:16 +0200

    Updates code in pages to allow navigation on site.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react"
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <nav className='ui fixed inverted menu'>
         <div className="ui container">
-            <h3 className="header item">
-                My Portfolio
+            <Link className="header item" to='/'>My Portfolio</Link>>
+            <div className="right menu">
+                <NavLink className="ui item" activeStyle={{ fontWeight: "bold" }} to='/about'>About Me</NavLink>
+                <NavLink className="ui item" activeStyle={{ fontWeight: "bold" }} to='/projects'>Projects</NavLink>
+            </div>
                 </h3> 
         </div>
     </nav>
