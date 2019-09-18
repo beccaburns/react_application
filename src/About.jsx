490c04af090b8876cd40b4179c7ca8d394f49b56
Becca <beccaburns89@gmail.com>
2019-09-18 08:33:25 +0200

    Updates content in About file.

--- src/About.jsx
+++ src/About.jsx
@@ -9,9 +9,15 @@ const About = () => {
         <img class="ui medium circular image" src="../src/img/profile.jpeg"></img>
         </div>
         <div className="column">
-          <h1>About Me</h1>
-          <p>Hello! My name is Becca. I am a student at the coding bootcamp, CraftAcademy, in Stockholm. 
-            In November I will pursue a position as a full stack web developer.</p>
+          <h1>Hey! I'm Becca.</h1>
+          <p>
+            An entrepreneur, writer, coffee-lover and currently, a student at the coding bootcamp, 
+            CraftAcademy, in Stockholm.
+            </p>
+            <br></br>
+            <p>
+            After years of working in digital marketing and sales, I am working to combine my business acumen with 
+            newly acquired technical skills in programming to pursue a role as a full stack web developer.</p>
             <br></br>
             <p>This profile will show you some of my latest projects with CraftAcademy and some of my other 
             projects in SEO & Paid Advertising.
