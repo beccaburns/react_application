2095b1b7c2815bffd472a2377a704abe776c9d4c
Becca <beccaburns89@gmail.com>
2019-09-18 08:47:49 +0200

    Updates to design.

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Hello = () => {
   return (
       <div className="ui main container">
-          <img class="ui fluid image" src="../src/img/becca.png"></img>
+          <img class="ui fluid image" src="../src/img/burns.png"></img>
       </div>
     
   )
